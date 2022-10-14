a05bd56d9c03813731c14658b1d254f0d309bf91
sifi6027 <sifi6027@gmail.com>
2022-10-14 03:46:46 +0100

    s

---
+++ strlcat.c
@@ -0,0 +1,15 @@
+size_t strlcat(char * restrict dst, const char * restrict src, size_t dstsize)
+{
+int i;
+i = 0;
+
+    while (srclen < destlen && dstsize > i)
+    {
+        /* code */
+    }
+    
+}
+int main() {
+
+  return 0;
+}
