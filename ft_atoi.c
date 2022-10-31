66947926c95476563422608a68d6af540ebe5f8f
sifi6027 <sifi6027@gmail.com>
2022-10-31 11:44:00 +0100

    00

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,13 +6,13 @@
 /*   By: aoutifra <aoutifra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/30 10:05:21 by aoutifra          #+#    #+#             */
-/*   Updated: 2022/10/30 20:48:15 by aoutifra         ###   ########.fr       */
+/*   Updated: 2022/10/31 11:40:28 by aoutifra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libc.h"
 
-int ft_atoi(const char *str)
+int    ft_atoi(const char *str)
 {
     unsigned long i;
     int sign;
